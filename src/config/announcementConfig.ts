d535644053e824b4dd5f145358373d33188a6094
Xwei2051 <1197170143@qq.com>
2026-07-21 20:41:27 +0800

    announcementConfig edit

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -3,7 +3,7 @@ import type { AnnouncementConfig } from "../types/config";
 // 公告栏配置
 export const announcementConfig: AnnouncementConfig = {
 	title: "", // 公告标题，填空使用i18n字符串Key.announcement
-	content: "Xwei2051 的 Signal Archive：这里会把问题、工具和生活现场整理成可回看的记录。", // 公告内容
+	content: "Xwei2051 的 blog：这里会把问题、工具和生活现场整理成可回看的记录。", // 公告内容
 	closable: true, // 允许用户关闭公告
 	link: {
 		enable: true, // 启用链接
